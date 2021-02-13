7ef50e09c8358d75691b21b04efc9d6da36765f0
Yunyoung Chung <double.y.0525@gmail.com>
2021-02-13 22:26:12 +0900

    change to factory pattern

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,41 @@
+from apispec import APISpec
+# from apispec_webframeworks.flask import FlaksPlugin
+from apispec.ext.marshmallow import MarshmallowPlugin
 from flask import Flask
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_apispec.extension import FlaskApiSpec
 import os
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-pjt_dir = os.getcwd()
-app.config.from_pyfile(f'{pjt_dir}/main/default.cfg')
+
+pjt_dir = f'{os.getcwd()}/main/default.cfg'
+
+docs = FlaskApiSpec()
+db = SQLAlchemy()
+api = Api()
+
+def create_app(config_folder=pjt_dir):
+    app = Flask(__name__)
+    app.config.from_pyfile(pjt_dir)
+    app.config.update({
+        'APISPEC_SPEC': APISpec(
+            title='Board CRUD',
+            version='v1',
+            openapi_version='2.0',
+            plugins=[MarshmallowPlugin()]
+        ),
+        'APISPEC_SWAGGER_UI_URL': '/docs/'
+    })
+
+    docs.init_app(app)
+    db.init_app(app)
+    api.init_app(app)
+
+    docs.register_existing_resources()
+
+    for k, v in docs.spec._paths.items():
+        docs.spec._paths[k] = {
+            inner_key: inner_value for inner_key, inner_value in v.items() in inner_key != "options"
+        }
+
+    return app
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from main import app
-from main.controllers import board
+from main import create_app
+import sys
+
 if __name__ == '__main__':
-  app.run(debug=True)
+    app = create_app()
+    app.run(debug=True, port=sys.argv[1])
